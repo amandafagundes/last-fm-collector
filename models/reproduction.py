8079124b90c9ef1ea258ce1109a1c13d51bc937d
Amanda Fagundes <amanda.fagundes@ledstartup.studio>
2021-01-06 16:19:07 -0300

    pynamodb added; reproductions endpoint created with pagination

--- models/reproduction.py
+++ models/reproduction.py
@@ -3,12 +3,12 @@ from datetime import datetime
 from pynamodb.models import Model
 from pynamodb.attributes import (UnicodeAttribute, NumberAttribute, UTCDateTimeAttribute,ListAttribute)
 from models.track import Track
-
-class Reproduction(Model):
+from models.base_model import BaseModel
+class Reproduction(BaseModel):
   class Meta:
     table_name = os.environ['DYNAMODB_TABLE']
 
   user_id = UnicodeAttribute(hash_key=True)
   reproduction = NumberAttribute(range_key=True)
-  created_at = UTCDateTimeAttribute(default=datetime.today().strftime('%Y-%m-%d %H:%M:%S.%f'))
+  created_at = UnicodeAttribute(default=datetime.today().strftime('%Y-%m-%d %H:%M:%S.%f'))
   tracks = employees = ListAttribute(of=Track)
